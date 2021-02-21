86d341f12e87084fe1b9997443c865de276bc96b
gearprn <gearprn@gmail.com>
2021-02-22 02:59:18 +0700

    :hammer: Add form filter in launches page

--- src/pages/Launches.js
+++ src/pages/Launches.js
@@ -3,31 +3,106 @@ import { fetchLaunches } from "../api/index";
 
 import LaunchCard from "../components/LaunchCard";
 
+const years = [
+  2006,
+  2007,
+  2008,
+  2009,
+  2010,
+  2011,
+  2012,
+  2013,
+  2014,
+  2015,
+  2016,
+  2017,
+  2018,
+  2019,
+  2020,
+];
+
 const Launches = () => {
   const [offset, setOffset] = useState(0);
+  const [selectYear, setSelectYear] = useState(2006);
+  const [launchStatus, setLaunchStatus] = useState("Default");
   const [launchesData, setLaunchesData] = useState([]);
+
   useEffect(() => {
     const fetchLaunchesData = async () => {
       await fetchLaunches(6, offset).then((res) => {
         setLaunchesData(res);
-        // setLaunchesData(JSON.stringify(res, undefined, 2));
       });
     };
     fetchLaunchesData();
   }, [offset]);
 
   const renderLaunchCards = launchesData.map((record) => {
-    console.log(record);
     return <LaunchCard key={record.flight_number} data={record} />;
   });
+
+  const options = years.map((year, index) => {
+    return (
+      <option key={index} value={`${year}`}>
+        {year}
+      </option>
+    );
+  });
+
+  const handleYearChange = (e) => {
+    setSelectYear(e.target.value);
+  };
+
+  const handleLaunchStatusClick = (e) => {
+    if (launchStatus === "Default" && e.target.innerHTML === "👈🏻") {
+      setLaunchStatus("Failed");
+    } else if (launchStatus === "Failed" && e.target.innerHTML === "👈🏻") {
+      setLaunchStatus("Success");
+    } else if (launchStatus === "Success" && e.target.innerHTML === "👈🏻") {
+      setLaunchStatus("Default");
+    } else if (launchStatus === "Default" && e.target.innerHTML === "👉🏻") {
+      setLaunchStatus("Success");
+    } else if (launchStatus === "Success" && e.target.innerHTML === "👉🏻") {
+      setLaunchStatus("Failed");
+    } else if (launchStatus === "Failed" && e.target.innerHTML === "👉🏻") {
+      setLaunchStatus("Default");
+    }
+  };
 
   return launchesData === "" ? (
     "fetching ..."
   ) : (
     <div>
-      <strong>
-        <h6 className="mb-3">🔥 Launches //</h6>
-      </strong>
+      <div className="flex">
+        <strong>
+          <h6 className="mb-3">🔥 Launches //</h6>
+        </strong>
+        <div className="flex ml-auto">
+          <form className="ml-2">
+            <label>
+              Rocket Name
+              <input
+                type="text"
+                name="name"
+                className="ml-2"
+                autoComplete="off"
+              />
+            </label>
+            <label> Choose a year: </label>
+
+            <select name="years" id="years" onChange={handleYearChange}>
+              {options}
+            </select>
+          </form>
+          <p className="ml-2">Launch Status: </p>
+          <p className="ml-2 cursor-pointer" onClick={handleLaunchStatusClick}>
+            👈🏻
+          </p>
+          <p className="w-24 text-center"> {launchStatus} </p>
+          <p className="cursor-pointer" onClick={handleLaunchStatusClick}>
+            👉🏻
+          </p>
+        </div>
+      </div>
       <div className="flex flex-col md:flex-row flex-wrap">
         {renderLaunchCards}
       </div>

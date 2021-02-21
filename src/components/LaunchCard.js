efff43e2fa2cb9ec911e10eeb8dab6f29314066c
gearprn <gearprn@gmail.com>
2021-02-22 04:54:10 +0700

    :lips: Update LaunchCard components

--- src/components/LaunchCard.js
+++ src/components/LaunchCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { formatDate } from "../utils/DateFormat";
 
 const LaunchCard = (props) => {
@@ -7,32 +8,37 @@ const LaunchCard = (props) => {
     launch_date_unix,
     launch_success,
     links: { mission_patch_small },
+    flight_number,
   } = props.data;
 
   return (
     <div className="p-2 w-full md:w-1/2 lg:w-1/3">
-      <div className="relative p-3 rounded-md bg-white dark:bg-gray-150 shadow-md h-56 overflow-hidden">
-        <img
-          className="absolute w-40 bottom-16 right-2 sm:bottom-6 md:bottom-14 md:w-36 lg:bottom-16 lg:w-36"
-          src={mission_patch_small}
-          alt=""
-        />
-        <div className="absolute bottom-3">
-          <p className="text-xl mb-3">{mission_name}</p>
-          <div className="flex justify-between gap-5">
-            <div>
-              <p>Launch on</p>
-              <p>{formatDate(launch_date_unix)}</p>
-            </div>
-            <div>
-              <p>Launch status</p>
-              <p>{launch_success ? "Success" : "Failed"}</p>
+      <Link to={`/launch/${flight_number}`}>
+        <div className="relative p-3 rounded-md bg-white dark:bg-gray-150 shadow-md h-56 overflow-hidden">
+          <img
+            className="absolute w-40 bottom-16 right-2 sm:bottom-6 md:bottom-14 md:w-36 lg:bottom-16 lg:w-36"
+            src={mission_patch_small}
+            alt=""
+          />
+          <div className="absolute bottom-3">
+            <p className="text-xl mb-3">{mission_name}</p>
+            <div className="flex justify-between gap-5">
+              <div>
+                <p className="text-sm">Launch on</p>
+                {/* <strong> */}
+                <p>{formatDate(launch_date_unix)}</p>
+                {/* </strong> */}
+              </div>
+              <div>
+                <p className="text-sm">Launch status</p>
+                {/* <strong> */}
+                <p>{launch_success ? "Success" : "Failed"}</p>
+                {/* </strong> */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      {/* <div className=""></div> */}
+      </Link>
     </div>
   );
 };

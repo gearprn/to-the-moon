76cbd46a53190075f0b6f70294bfd78b80553126
Icyscools <meet.ice1@gmail.com>
2021-02-22 04:11:09 +0700

    :zap: Add link to rocker detail

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -1,25 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RocketCard = ({ data }) => {
-  const { rocket_name, company, flickr_images } = data;
+  const { rocket_id, rocket_name, company, active, flickr_images } = data;
 
   return (
     <div className='p-2 w-full md:w-1/2 lg:w-1/3'>
-      <div className='relative p-3 rounded-md bg-white dark:bg-gray-150 shadow-md h-56 overflow-hidden'>
-        <img
-          className='absolute top-0 left-0 w-full'
-          src={flickr_images[0]}
-          alt=''
-        />
-        <div className='absolute bottom-3'>
-          <h1>{rocket_name}</h1>
-          <div className='flex justify-between gap-5'>
-            <div>
-              <p>{company}</p>
-            </div>
+      <Link to={`/rockets/${rocket_id}`}>
+        <div className='relative p-3 rounded-md bg-white dark:bg-gray-150 h-64 shadow-md overflow-hidden cursor-pointer'>
+          <img
+            className='absolute top-0 left-0 h-2/3 w-full'
+            src={flickr_images[0]}
+            alt=''
+          />
+          <div className='absolute w-full bottom-3'>
+            <h1 className='text-xl'>{rocket_name}</h1>
+            <p className='text-base'>{company}</p>
+            <p className='text-sm'>{active ? "Active" : "Inactive"}</p>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/pages/App.js
+++ src/pages/App.js
@@ -15,14 +15,14 @@ function App() {
     <ThemeProvider>
       <Router>
         <Navbar />
-        <div className="App bg-gray-100 dark:bg-gray-200 text-dark dark:text-white">
-          <div className="container px-4 mx-auto pt-5">
+        <div className='App bg-gray-100 dark:bg-gray-200 text-dark dark:text-white'>
+          <div className='container px-4 mx-auto pt-5'>
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/launches" component={Launches} />
-              <Route exact path="/launch-detail" component={LaunchDetail} />
-              <Route exact path="/rockets" component={Rockets} />
-              <Route exact path="/rocket-detail" component={RocketDetail} />
+              <Route exact path='/' component={Home} />
+              <Route exact path='/launches' component={Launches} />
+              <Route exact path='/launch-detail' component={LaunchDetail} />
+              <Route exact path='/rockets' component={Rockets} />
+              <Route exact path='/rockets/:rocketId' component={RocketDetail} />
             </Switch>
           </div>
         </div>
